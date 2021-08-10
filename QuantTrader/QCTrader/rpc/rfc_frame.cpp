57b06b6d4a6498005acc00ca43d64c00e841965d
yuanhuei <896521035@qq.com>
2021-08-10 22:48:03 +0800

    update

--- QuantTrader/QCTrader/rpc/rfc_frame.cpp
+++ QuantTrader/QCTrader/rpc/rfc_frame.cpp
@@ -1,4 +1,12 @@
 #include "rfc_frame.h"
+#include <msgpack.hpp>
+#include<qdatetime.h>
+#include<iostream>
+#include<windows.h>
+
+#define KEEP_ALIVE_TOPIC "_keep_alive"
+#define KEEP_ALIVE_INTERVAL 1 //1Ãë
+#define KEEP_ALIVE_TOLERANCE 30 //30Ãë
 
 void generate_certificates(std::string strname)
 {
@@ -9,7 +17,7 @@ RpcServer::RpcServer()
 {
 	m_socket_rep = new zmq::socket_t(m_zmqcontext, zmq::socket_type::rep);
 	m_socket_pub = new zmq::socket_t(m_zmqcontext, zmq::socket_type::pub);
-
+	//msgpack::type::tuple
 
 }
 
@@ -24,7 +32,7 @@ bool RpcServer::isActive()
 	return m_active;
 }
 
-void RpcServer::start(std::string rep_address, std::string pub_address, std::string str_username,std::string str_password, std::string server_secretkey_path)
+void RpcServer::start(std::string rep_address, std::string pub_address)
 {
 	if (m_active)
 		return;
@@ -33,6 +41,7 @@ void RpcServer::start(std::string rep_address, std::string pub_address, std::str
 	m_socket_pub->bind(pub_address);
 	m_active = true;
 	m_thread = std::thread::thread(std::bind(&RpcServer::run, this));
+	//zmq:;zmq_poll
 
 }
 void RpcServer::stop()
@@ -42,14 +51,42 @@ void RpcServer::stop()
 }
 void RpcServer::join()
 {
-
+	m_thread.join();
 }
 void RpcServer::run()
 {
+	QDateTime start_time = QDateTime::currentDateTime();
+	while (m_active)
+	{
+		QDateTime cur_time = QDateTime::currentDateTime();
+		int delta = cur_time.secsTo(start_time);
 
+		if (delta > KEEP_ALIVE_INTERVAL)
+			publish(KEEP_ALIVE_TOPIC, cur_time.toString().toStdString());
+		zmq_pollitem_t items[1];
+		/* First item refers to socket 'socket' */
+		items[0].socket = m_socket_rep;
+		items[0].events = ZMQ_POLLIN;
+		//Use poll to wait event arrival, waiting time is 1 second(1000 milliseconds)
+		int rc = zmq_poll(items, 1, 1000);
+		if (rc = 0)
+			continue;
+		zmq::message_t msg;
+		m_socket_rep->recv(&msg);
+		std::cout << "Received Hello" << std::endl;
+
+		//sleep(1);
+
+		zmq::message_t reply(5);
+		memcpy(reply.data(), "World", 5);
+		m_socket_rep->send(reply);
+	}
 }
-void RpcServer::publish()
+void RpcServer::publish(std::string strTopic, std::string strData)
 {
+	m_threadMutex.lock();
+	m_socket_pub->send((void*)&(strTopic.c_str()), zmq::send_flags::dontwait);
+	m_threadMutex.unlock();
 
 }
 void RpcServer::fun_register()
@@ -59,6 +96,8 @@ void RpcServer::fun_register()
 
 RpcClient::RpcClient()
 {
+	m_socket_req = new zmq::socket_t(m_zmqcontext, zmq::socket_type::req);
+	m_socket_sub = new zmq::socket_t(m_zmqcontext, zmq::socket_type::sub);
 
 
 }
@@ -68,17 +107,24 @@ RpcClient::~RpcClient()
 
 }
 
-void RpcClient::start()
+void RpcClient::start(std::string req_address,std::string sub_address)
 {
+	if (m_active)
+		return;
 
+	m_socket_req->connect(req_address);
+	m_socket_sub->connect(sub_address);
+	m_active = true;
+	m_thread = std::thread::thread(std::bind(&RpcServer::run, this));
 }
 void RpcClient::stop()
 {
+	m_active = false;
 
 }
 void RpcClient::join()
 {
-
+	m_thread.join();
 }
 void RpcClient::run()
 {
@@ -88,11 +134,11 @@ void RpcClient::callback()
 {
 
 }
-void RpcClient::subscribe_topic()
+void RpcClient::subscribe_topic(std::string strTopic)
 {
-
+	//m_socket_sub->setsockopt()
 }
 void RpcClient::on_disconnected()
 {
-
+	std::cout<<"RpcServer has no response over {tolerance} seconds, please check you connection.";
 }
--- QuantTrader/QCTrader/rpc/rfc_frame.h
+++ QuantTrader/QCTrader/rpc/rfc_frame.h
@@ -4,9 +4,10 @@
 #include<string>
 #include<zmq.hpp>
 #include<zmq_addon.hpp>
-#include<nanomsg/reqrep.h>
+
 #include<map>
 #include<thread>
+#include<mutex>
 
 void generate_certificates(std::string strname);
 
@@ -17,11 +18,11 @@ public:
 	~RpcServer();
 
 	bool isActive();
-	void start(std::string rep_address, std::string pub_address, std::string str_username, std::string str_password, std::string server_secretkey_path);
+	void start(std::string rep_address, std::string pub_address);
 	void stop();
 	void join();
 	void run();
-	void publish();
+	void publish(std::string strTopic,std::string strData);
 	void fun_register();
 
 
@@ -34,19 +35,16 @@ private:
 	zmq::context_t m_zmqcontext;
 
 	//# Reply socket(Request每reply pattern)
-	zmq::socket_t* m_socket_rep; //zmq.Socket = self.__context.socket(zmq.REP)
+	zmq::socket_t* m_socket_rep;
 	
 	//# Publish socket(Publish每subscribe pattern)
-		
-	zmq::socket_t* m_socket_pub;// zmq.Socket = self.__context.socket(zmq.PUB)
+	zmq::socket_t* m_socket_pub;
 
 		//# Worker thread related
 	bool m_active =false;//                     # RpcServer status
 	std::thread  m_thread; // m_thread: threading.Thread = None          # RpcServer thread
-	//std::thread m_lock: threading.Lock = threading.Lock()
+	std::mutex m_threadMutex;
 
-	//# Authenticator used to ensure data security
-	//m_authenticator: ThreadAuthenticator = None
 
 };
 class RpcClient
@@ -55,17 +53,32 @@ public:
 	RpcClient();
 	~RpcClient();
 
-	void start();// std::string rep_address, std::string pub_address, std::string str_username, std::string str_password, std::string server_secretkey_path);
+	void start(std::string req_address, std::string sub_address);// std::string rep_address, std::string pub_address, std::string str_username, std::string str_password, std::string server_secretkey_path);
 	void stop();
 	void join();
 	void run();
 	void callback();
-	void subscribe_topic();
+	void subscribe_topic(std::string strTopic);
 	void on_disconnected();
 
 
 private:
+	//# Save functions dict : key is fuction name, value is fuction object
+	std::map < std::string, int >	m_functions;// : Dict[str, Any] = {}
 
+	//# Zmq port related
+	zmq::context_t m_zmqcontext;
+
+	//# Reply socket(Request每reply pattern)
+	zmq::socket_t* m_socket_req;
+
+	//# Publish socket(Publish每subscribe pattern)
+	zmq::socket_t* m_socket_sub;
+
+	//# Worker thread related
+	bool m_active = false;//                     # RpcServer status
+	std::thread  m_thread; // m_thread: threading.Thread = None          # RpcServer thread
+	std::mutex m_threadMutex;
 
 };
 #endif // !RFC_FRAME_H
